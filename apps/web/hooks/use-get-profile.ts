1d6c0e60ba58727f4cb4e6259a1d9b6f7665cef9
TAEWOONG KANG <xodndxnxn@gmail.com>
2023-03-07 00:07:02 +0900

    Feat: use-get-profile hooks 제작

---
+++ apps/web/hooks/use-get-profile.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState, useResetRecoilState } from 'recoil';
+
+import { userState } from '@recoilState';
+import { onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { auth, db } from '@config';
+
+export const useGetProfile = () => {
+  const [user, setUser] = useRecoilState(userState);
+  const resetUser = useResetRecoilState(userState);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const isUserExist = !!auth.currentUser;
+
+  useEffect(() => {
+    const unlisten = onAuthStateChanged(auth, async fbUser => {
+      setIsLoading(true);
+      if (fbUser && user === null) {
+        const { uid } = fbUser;
+        const firestoreUser: User = await getUserProfile(uid);
+
+        setUser({
+          uid,
+          ...firestoreUser,
+        });
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
+    });
+    return () => {
+      unlisten();
+    };
+  }, [user]);
+
+  const getUserProfile = async (id: string) => {
+    const userDocRef = doc(db, 'User', id);
+    const userData = (await getDoc(userDocRef)).data() as User;
+
+    return { ...userData };
+  };
+
+  return { user, resetUser, isUserExist, isLoading };
+};
