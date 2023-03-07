abae484ad8c55b17d358333f097c7a881360207b
TAEWOONG KANG <xodndxnxn@gmail.com>
2023-03-07 17:21:04 +0900

    Delete: 로그아웃 시에는 로딩 X

--- apps/web/templates/Settings/Settings.tsx
+++ apps/web/templates/Settings/Settings.tsx
@@ -1,29 +1,20 @@
-import { useState } from 'react';
-
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
-
-import { Loading } from '@components';
 
 import { auth } from '@config';
 
 import { useGetProfile } from '@hooks';
 
 export const Settings = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
   const { resetUser } = useGetProfile();
   const router = useRouter();
 
   const logout = async () => {
-    setIsLoading(true);
     await signOut(auth).then(() => {
       resetUser();
     });
-    router.replace('/').then(() => setIsLoading(false));
+    router.replace('/');
   };
-
-  if (isLoading) return <Loading />;
 
   return (
     <main className='flex h-screen flex-col items-center justify-center'>
