727427212795bda50f946c64d9e4e9887f246cb4
TAEWOONG KANG <xodndxnxn@gmail.com>
2023-03-07 16:37:12 +0900

    Delete: 백업 DB 사용 X

--- apps/web/config/firebase.ts
+++ apps/web/config/firebase.ts
@@ -15,23 +15,9 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Your web app's Firebase configuration
-const backupFirebaseConfig = {
-  // KEY값 입력
-  apiKey: 'AIzaSyBY4NKBf6D6_8ZIp6gQ8r2SSCuMEXF5RxI',
-  authDomain: 'alda-2fa29.firebaseapp.com',
-  projectId: 'alda-2fa29',
-  storageBucket: 'alda-2fa29.appspot.com',
-  messagingSenderId: '538747837663',
-  appId: '1:538747837663:web:e19e0f9ab2ef327d325d6a',
-  measurementId: 'G-DXR1M5FY5Z',
-};
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const backupApp = initializeApp(backupFirebaseConfig, 'secondary');
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const backupDB = getFirestore(backupApp);
 export const storage = getStorage(app);
 export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
