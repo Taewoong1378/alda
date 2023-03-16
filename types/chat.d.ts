4d17cf4ec8f8e787726290ae904347a43abfb428
TAEWOONG KANG <xodndxnxn@gmail.com>
2023-03-16 22:41:33 +0900

    Add: chat type 추가

---
+++ types/chat.d.ts
@@ -0,0 +1,5 @@
+interface Chat {
+  user_name: string;
+  userfeeling_big: string;
+  userfeeling_small: string[];
+}
--- types/index.ts
+++ types/index.ts
@@ -2,5 +2,6 @@
 /// <reference path="./user.d.ts" />
 /// <reference path="./location.d.ts" />
 /// <reference path="./mood.d.ts" />
+/// <reference path="./chat.d.ts" />
 
 export {};
